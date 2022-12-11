989ad927cce2d53b426908b2b6abbe29e203d401
Daniel Booy <deinonychai@gmail.com>
2022-12-11 04:30:53 +0000

    Started printed reporter citations

--- mcgill_jurisprudence_rules.py
+++ mcgill_jurisprudence_rules.py
@@ -57,15 +57,30 @@ def generate_case_citation(url: str) -> str:
         neutral_citation_list = data["citation"].split()
         neutral_citation = " ".join(neutral_citation_list[:3])
         # Adds the SCR printed citation whenever it's available
+        # This accords with the official reporter hierarchy in McGill 9e 3.1
         if "SCR" in parsed_citation:
             pinpoint = enter_pinpoint()
-            print_citation = " ".join(neutral_citation_list[-4:-1])
+            official_reporter = " ".join(neutral_citation_list[-4:-1])
             citation = f"*{style_of_cause}*, {neutral_citation}, "\
-                       f"{print_citation} at para {pinpoint}."
+                       f"{official_reporter} at para {pinpoint}."
         else:
             pinpoint = enter_pinpoint()
             citation = f"*{style_of_cause}*, {neutral_citation} at para "\
                        f"{pinpoint}."
         return citation
     else:
-        return "Neutral citation not found."
+        generate_print_reporter_citations()
+
+def generate_print_reporter_citations() -> str:
+    '''
+    Generates a printed reporter citation.
+    '''
+    printed_reporter_string = "[2017] SCJ No 60 (QL) — EYB 2017-287963 — "\
+        "142 WCB (2d) 343 — 396 CRR (2d) 212 — 357 CCC (3d) 350 — "\
+        "42 CR (7th) 74 — [2017] EXP 3391 — 418 DLR (4th) 382"
+    if " — " in printed_reporter_string:
+        print(printed_reporter_string.split(" — "))
+    else:
+        print(printed_reporter_string.split(", "))
+
+    return "Neutral citation not found."
