db92027cac50cd8361eb7e2c6827e3760c593946
Bob Belderbos <bobbelderbos@gmail.com>
2023-01-24 14:49:03 +0100

    load db from config as well

--- citator/settings.py
+++ citator/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 
 from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,10 +73,9 @@ WSGI_APPLICATION = 'citator.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
