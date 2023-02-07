717863608c2fe4f80e7b12e3c06ff418f624df45
Bob Belderbos <bobbelderbos@gmail.com>
2023-02-07 15:07:50 +0100

    cleaned up navigation bar, deleted commented code

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('changelog/', views.changelog, name='changelog'),
     path('process_text/', views.process_text, name='process_text'),
-#    path('<int:pk>/', views.changelog_detail, name='changelog_detail'),
 ]
 
